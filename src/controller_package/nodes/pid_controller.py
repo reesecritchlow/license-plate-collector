bf87f16a91356fe0799c8a895d34f25a96a6f061
trlee02 <tristan.rene.lee@gmail.com>
2022-11-22 16:13:13 -0800

    tweeked some pid values

--- src/controller_package/nodes/pid_controller.py
+++ src/controller_package/nodes/pid_controller.py
@@ -11,7 +11,7 @@ from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
-COL_CROP_RATIO = 3/8
+COL_CROP_RATIO = 5/8
 ROW_RATIO = 6/8
 
 class pid_controller:
@@ -29,6 +29,7 @@ class pid_controller:
 
         rows = cv_image.shape[0]
         cols = cv_image.shape[1]
+
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         lower_white = np.array([0,0,125], dtype=np.uint8)
         upper_white = np.array([0,0,255], dtype=np.uint8)
@@ -36,21 +37,20 @@ class pid_controller:
         mask = cv2.inRange(hsv, lower_white, upper_white)
         res = cv2.bitwise_and(cv_image,cv_image, mask= mask)
         
-
-        h, s, res_v = cv2.split(res)
+        res_v = cv2.split(res)[2]
 
         crop = res_v[int(ROW_RATIO*rows):rows, int(cols*COL_CROP_RATIO):cols]
-        # blur = cv2.blur(gray,(9,9))
+        blur = cv2.blur(crop,(9,9))
         # _, binary = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY_INV)
         
 
-        contours, hierarchy = cv2.findContours(crop, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cx = 0
         cy = 0
 
         move = Twist()
-        m = 3
-        move.linear.x = 0.1
+        m = 10
+        move.linear.x = 0.3
         # move.angular.z = 0
 
         if len(contours) > 0:
@@ -61,18 +61,18 @@ class pid_controller:
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
 
-            cx_center = int(cx+cols*COL_CROP_RATIO/2-180)
+            cx_center = int(cx+cols*COL_CROP_RATIO/2)
             # print(f"{cx}, {cols*COL_CROP_RATIO}, {cols/2}, {cx+cols*COL_CROP_RATIO/2-200}")
-            # print(f"cx_center: {cx_center}")
+            print(f"cx_center: {cx_center}")
             print(1-2*(cx_center)/cols)
             move.angular.z = (1-2*(cx_center)/cols)*m
             cv2.circle(cv_image, (cx_center, cy+int(rows*ROW_RATIO)), 15, (0, 255, 255), 2)
             cv2.circle(crop, (cx, cy), 15, (0, 255, 255), 2)
 
         # cv2.imshow('img', bgr)
-        cv2.imshow("crop",crop)
         cv2.imshow('res',res)
         cv2.imshow('img', cv_image)
+        cv2.imshow("crop",crop)
         # cv2.imshow('binary',binary)
         cv2.waitKey(3)
 
