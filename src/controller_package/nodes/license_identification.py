d4a3de36736e67369179c7e57f36f54d23e233fc
trlee02 <tristan.rene.lee@gmail.com>
2022-11-28 12:51:57 -0800

    experimenting with HSV values to try and get the best contour of the license

--- src/controller_package/nodes/license_identification.py
+++ src/controller_package/nodes/license_identification.py
@@ -11,13 +11,13 @@ from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
-UPPER_WHITE = np.array([0,0,255], dtype=np.uint8)
-LOWER_WHITE = np.array([0,0,125], dtype=np.uint8)
+UPPER_WHITE = np.array([0,0,255*0.8], dtype=np.uint8)
+LOWER_WHITE = np.array([0,0,255*0.4], dtype=np.uint8)
 
 COL_CROP_RATIO = 5/8
 ROW_RATIO = 6/8
 
-class car_detector:
+class license_detector:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.image_callback)
@@ -28,9 +28,13 @@ class car_detector:
         except CvBridgeError as e:
             print(e)
 
-        cv_image = self.process_image(cv_image)
+        processed_img = self.process_image(cv_image)
+        processed_img = cv2.split(processed_img)[2]
+        contours, hierarchy = cv2.findContours(processed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(cv_image, contours, -1, (0,0,255), 3)
         
-        cv2.imshow(cv_image)
+        cv2.imshow('img',cv_image)
+        cv2.imshow('processed',processed_img)
         cv2.waitKey(3)
         # This method should just get the image and call other functions
     
--- src/controller_package/nodes/main_controller.py
+++ src/controller_package/nodes/main_controller.py
@@ -3,27 +3,23 @@
 import roslib
 import rospy
 import sys
-import timer_controller
-import direct_controller
 import cv2
-import time
-from pid_controller import pid_controller
-import timer_controller
-
+from license_identification import license_detector
 
 
 def main(args):
-    tc = timer_controller.timer_controller()
-    dc = direct_controller.direct_controller()
+    # tc = timer_controller.timer_controller()
+    # dc = direct_controller.direct_controller()
     rospy.init_node('main_controller', anonymous=True)
-
-    tc.start()
-    dc.spin(-90)
-    dc.drive(0.4, 0.3)  
-    dc.spin(90)
-    dc.drive(1, 0.3)
-    pid_c = pid_controller(tc)
+    ld = license_detector()
+    # tc.start()
+    # dc.spin(-90)
+    # dc.drive(0.4, 0.3)  
+    # dc.spin(90)
+    # dc.drive(1, 0.3)
+    # pid_c = pid_controller(tc)
     # tc.terminate()
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
